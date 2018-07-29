681a7c96f7d31b46a9101be94dda5391c88dabfb
andela-oaladeusi <olawale.aladeusi@andela.com>
2018-07-29 20:56:48 +0100

    Add postgresql service to travis

--- src/config.py
+++ src/config.py
@@ -8,6 +8,7 @@ class Development(object):
     """
     DEBUG = True
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 

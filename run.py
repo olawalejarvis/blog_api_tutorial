7c5ec9963b0bb09aaad7d5b6fd09154eaf675bd0
andela-oaladeusi <olawale.aladeusi@andela.com>
2018-07-29 19:52:30 +0100

    Add postgresql service to travis

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from src.app import create_app
 
 if __name__ == '__main__':
   env_name = os.getenv('FLASK_ENV')
-  port = os.getenv('PORT')
   app = create_app(env_name)
   # run app
-  app.run(port=port)
+  app.run(host='0.0.0.0')
